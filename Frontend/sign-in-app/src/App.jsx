2edfac0169885de733fbf247667e8d2fa374dc8e
SebaS180304 <drdiegoramos18@gmail.com>
2025-03-13 04:21:04 -0600

    Cambios de Rutas

--- Frontend/sign-in-app/src/App.jsx
+++ Frontend/sign-in-app/src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import Home from './pages/Home';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Lecture from './pages/Lecture';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
 function App() {
     return (
-        <div>
+        <Router>
             <Routes>
               <Route path='/' element={<Login />} />
               <Route path='/learn' element={<Home />} />
               <Route path='/lecture' element={<Lecture />} />
               <Route path='/dashboard' element={<Dashboard />} />
             </Routes>
-        </div>
+        </Router>
     );
 }
 
