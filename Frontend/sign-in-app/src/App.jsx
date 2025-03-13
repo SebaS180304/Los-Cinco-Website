dc95a2b645daddf6a46797e9e4f01c4acccbedd7
SebaS180304 <drdiegoramos18@gmail.com>
2025-03-12 23:28:50 -0600

    Update final al archivo App,jsx
    Se eliminó la ruta hacia la página learn.

--- Frontend/sign-in-app/src/App.jsx
+++ Frontend/sign-in-app/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
-import Learn from './pages/Learn';
 import Lecture from './pages/Lecture';
 import Login from './pages/Login';
 
